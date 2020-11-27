8d39a4a17980276ea7eddbbe9c0b3ffe4067ae87
yoyohobc <38155744+yoyohobc@users.noreply.github.com>
2020-11-27 16:19:59 +0800

    Update python_practice.py

--- python_practice.py
+++ python_practice.py
@@ -1,4 +1,4 @@
-#二分搜尋法
+#二分搜尋法O(log n)
 nums = [3,6,9,15,22,45,52,68,77,84,96,108,118]
 find = 45
 low = 0
@@ -27,4 +27,17 @@ for i in range(length):
             
     Numbers[i],Numbers[min_index] = Numbers[min_index],Numbers[i]
     
+print(Numbers)
+#插入排序法O(n^2)
+Numbers = [41,33,17,80,61,5,55]
+length = len(Numbers)
+for i in range(1, length):
+    position = i
+    value = Numbers[i]
+    print('\n'+str(i))
+    while position > 0 and Numbers[position-1] > Numbers[position]:
+        print('P:',position)
+        Numbers[position], Numbers[position-1] = Numbers[position-1], Numbers[position]
+        position -= 1
+        print(Numbers)
 print(Numbers)
